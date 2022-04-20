65b4330678b686afff44ee0d433b062cf4c09d05
awqwqwq <2119392819@qq.com>
2022-04-20 16:55:35 +0800

    second

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import IndexRouter from "./router/IndexRouter";
-import './App.css';
-function App() {
-  return <div className="app">
-      <IndexRouter></IndexRouter>
-  </div>
-}
-
-export default App;
